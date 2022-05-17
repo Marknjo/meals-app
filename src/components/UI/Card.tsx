304de9facc877f4232f5d2e5ba7431d4ce6b96f1
Marknjo <marknjo7@gmail.com>
2022-05-17 13:22:53 +0300

    Add flexibility to Card component styler to allow custom margins and background colors

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -3,11 +3,16 @@ import styles from './Card.module.css';
 interface CardProps {
   children: React.ReactNode;
   bgStyle: string;
+  pdStyle?: string;
 }
 
 const Card = (props: CardProps) => {
   return (
-    <div className={`${styles.card} ${styles[`card--${props.bgStyle}`]}`}>
+    <div
+      className={`${styles.card} ${styles[`card--${props.bgStyle}`]}  ${
+        props.pdStyle ? styles[`card--${props.pdStyle}`] : ''
+      }`}
+    >
       {props.children}
     </div>
   );
