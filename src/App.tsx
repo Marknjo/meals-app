7f42e617d28ae2ec233b996ce213707711cf0aa0
Marknjo <marknjo7@gmail.com>
2022-05-17 12:15:51 +0300

    Add showing of meals items summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from './components/Layouts/Header';
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 };
---
+++ src/components/Meals/Meals.tsx
@@ -0,0 +1,14 @@
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <>
+      {/* Meals Summary */}
+      <MealsSummary />
+
+      {/* Meals Items */}
+    </>
+  );
+};
+
+export default Meals;
